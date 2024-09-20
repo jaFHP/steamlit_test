2a7673f4f6fab0b02b3a8dfe93ce6ce31bc2db58
jaFHP <167227411+jaFHP@users.noreply.github.com>
2024-09-20 07:03:50 +0000

    setting stuff up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as st
-st.title('Hellooo Woorld')
+import pandas as pd
+st.title('Hellooo Bastar Craton')
 st.write('**This is just a test for the Data Science Short Course @goetheUniFrankfurt**')
+
+#import Georoc File
+df=pd.read_csv('data/Bastar Craton.csv')
+st.dataframe(df)
